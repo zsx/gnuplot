1c4c9ee4a18bdc34649cb4c65f8fabd3f2c889b7
Shixin Zeng <zeng.shixin@gmail.com>
2009-07-26 16:11:44 -0500

    set default font search path on windows to C:/windows/fonts

--- src/deps/gd-2.0.35/gd.h
+++ src/deps/gd-2.0.35/gd.h
@@ -71,7 +71,11 @@ extern "C"
 /* 2.0.23: more Type 1 fonts */
 #ifndef DEFAULT_FONTPATH
 /* default fontpath for unix systems  - whatever happened to standards ! */
+#ifndef _WIN32
 #define DEFAULT_FONTPATH "/usr/X11R6/lib/X11/fonts/TrueType:/usr/X11R6/lib/X11/fonts/truetype:/usr/X11R6/lib/X11/fonts/TTF:/usr/share/fonts/TrueType:/usr/share/fonts/truetype:/usr/openwin/lib/X11/fonts/TrueType:/usr/X11R6/lib/X11/fonts/Type1:/usr/lib/X11/fonts/Type1:/usr/openwin/lib/X11/fonts/Type1"
+#else
+#define DEFAULT_FONTPATH "C:/WINDOWS/FONTS"
+#endif
 #endif
 #ifndef PATHSEPARATOR
 #define PATHSEPARATOR ":"
